ce54e4615a334c269667abbce79e890bc506d81f
Zin Min Phyoe <zinminphyo075@gmail.com>
2019-02-01 20:10:31 +0630

    first step implement

---
+++ VideoDownloaderFor_iOS/VideoDownloaderFor_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoDownloaderFor_iOS
+//
+//  Created by Zin Min Phyoe on 2/1/19.
+//  Copyright © 2019 Zin Min Phyoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VideoDownloaderFor_iOS/VideoDownloaderFor_iOSTests/VideoDownloaderFor_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  VideoDownloaderFor_iOSTests.swift
+//  VideoDownloaderFor_iOSTests
+//
+//  Created by Zin Min Phyoe on 2/1/19.
+//  Copyright © 2019 Zin Min Phyoe. All rights reserved.
+//
+
+import XCTest
+@testable import VideoDownloaderFor_iOS
+
+class VideoDownloaderFor_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
